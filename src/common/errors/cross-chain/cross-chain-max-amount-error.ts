1888938a21a9c174755c12f80633eb08d070fd86
Guji <kindaguji@gmail.com>
2022-03-11 21:57:39 -0500

    Pass min and max on error

--- src/common/errors/cross-chain/cross-chain-max-amount-error.ts
+++ src/common/errors/cross-chain/cross-chain-max-amount-error.ts
@@ -2,8 +2,10 @@ import { RubicSdkError } from '@common/errors/rubic-sdk.error';
 import BigNumber from 'bignumber.js';
 
 export class CrossChainMaxAmountError extends RubicSdkError {
+    max: BigNumber;
     constructor(maxAmount: BigNumber, tokenSymbol: string) {
         super(`Max amount is ${maxAmount.toFixed()} ${tokenSymbol}`);
         Object.setPrototypeOf(this, CrossChainMaxAmountError.prototype);
+        this.max = maxAmount;
     }
 }
--- src/common/errors/cross-chain/cross-chain-min-amount-error.ts
+++ src/common/errors/cross-chain/cross-chain-min-amount-error.ts
@@ -2,8 +2,10 @@ import { RubicSdkError } from '@common/errors/rubic-sdk.error';
 import BigNumber from 'bignumber.js';
 
 export class CrossChainMinAmountError extends RubicSdkError {
+    min: BigNumber;
     constructor(minAmount: BigNumber, tokenSymbol: string) {
         super(`Min amount is ${minAmount.toFixed()} ${tokenSymbol}`);
         Object.setPrototypeOf(this, CrossChainMinAmountError.prototype);
+        this.min = minAmount;
     }
 }
